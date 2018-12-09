fc6ea0e9b6cf74724c259c0cac0d28fe92ded90c
Jaewan Park <jaewan.huey.park@gmail.com>
2018-12-09 23:04:54 +0900

    Create socket for server

--- Source/Marsettler/MarsettlerGameModeBase.cpp
+++ Source/Marsettler/MarsettlerGameModeBase.cpp
@@ -1,4 +1,6 @@
 #include "MarsettlerGameModeBase.h"
+#include "Networking.h"
+#include "Sockets.h"
 #include "message/actor_generated.h"
 
 AMarsettlerGameModeBase::AMarsettlerGameModeBase()
@@ -7,7 +9,12 @@ AMarsettlerGameModeBase::AMarsettlerGameModeBase()
 	PrimaryActorTick.bCanEverTick = true;
 }
 
-void AMarsettlerGameModeBase::Tick(float DeltaTime)
+void AMarsettlerGameModeBase::BeginPlay()
+{
+	connectSocket(FIPv4Address(127, 0, 0, 1), 8080);
+}
+
+void AMarsettlerGameModeBase::Tick(float delta)
 {
 	flatbuffers::FlatBufferBuilder  builder;
 
@@ -20,5 +27,19 @@ void AMarsettlerGameModeBase::Tick(float DeltaTime)
 	auto readAcotr = message::GetActor(buf);
 	auto readPosition = readAcotr->Position();
 
-	UE_LOG(LogTemp, Warning, TEXT("Hello flatbuffers! %f: %f"), readPosition->X(), readPosition->Y());
+	UE_LOG(LogTemp, Log, TEXT("Hello flatbuffers! %f: %f"), readPosition->X(), readPosition->Y());
+}
+
+void AMarsettlerGameModeBase::connectSocket(const FIPv4Address& ip, const int32 port)
+{
+	socket = ISocketSubsystem::Get(PLATFORM_SOCKETSUBSYSTEM)->CreateSocket(NAME_Stream, TEXT("default"), false);
+
+	TSharedRef<FInternetAddr> addr = ISocketSubsystem::Get(PLATFORM_SOCKETSUBSYSTEM)->CreateInternetAddr();
+	addr->SetIp(ip.Value);
+	addr->SetPort(port);
+
+	if (socket->Connect(*addr))
+	{
+		UE_LOG(LogTemp, Log, TEXT("Socket connected."));
+	}
 }
--- Source/Marsettler/MarsettlerGameModeBase.h
+++ Source/Marsettler/MarsettlerGameModeBase.h
@@ -4,12 +4,24 @@
 #include "GameFramework/GameModeBase.h"
 #include "MarsettlerGameModeBase.generated.h"
 
+class FSocket;
+struct FIPv4Address;
+
 UCLASS()
 class MARSETTLER_API AMarsettlerGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+
+private:
+	FSocket* socket = nullptr;
+
+public:
 	AMarsettlerGameModeBase();
 
-	void Tick(float DeltaTime);
+	void BeginPlay() override;
+
+	void Tick(float delta) override;
+
+private:
+	void connectSocket(const FIPv4Address& ipStr, const int32 port);
 };
